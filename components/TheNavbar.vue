30125c2f43764895d4e4a485963c99ca984d8fd7
sglkc <31957516+sglkc@users.noreply.github.com>
2021-11-11 13:42:04 +0700

    fix navigation bar: add href to menu

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -23,9 +23,12 @@
         <v-list-item
           v-for="(menu, i) in menus"
           :key="i"
+          :href="menu.link"
           dense
         >
-          <v-icon class="pr-4">{{ menu.icon }}</v-icon>
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
           <v-list-item-title>{{ menu.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
